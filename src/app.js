a9474499d4a7afd9eb5223e07194d498125dc533
param gupta <paramgupta0305@gmail.com>
2025-09-07 19:23:54 +0530

    Playing with routing

--- src/app.js
+++ src/app.js
@@ -5,13 +5,20 @@ const app = express();
 
 
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello ");
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:"Param",lastName:"Gupta"});
 });
-
+app.post("/hello",(req,res)=>{
+    res.send("hello ji")
+})
+app.delete("/hello",(req,res)=>{
+    res.send("hello delete");
+})
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 });
+
 
 app.listen(3000,()=>{
     console.log("Server is successfull running")

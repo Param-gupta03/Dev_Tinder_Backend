c78e024f02ca8e116dc14e0c3665873ff18d9ae6
param gupta <paramgupta0305@gmail.com>
2025-09-09 19:52:11 +0530

    play with middleware to understand it

--- src/app.js
+++ src/app.js
@@ -6,18 +6,28 @@ const app = express();
 
 
 app.get("/user/:userId",(req,res)=>{
-    console.log(req.params);
+    console.log(req.params); 
     res.send({firstName:"Param",lastName:"Gupta"});
 });
-app.post("/hello",(req,res)=>{
-    res.send("hello ji")
-})
-app.delete("/hello",(req,res)=>{
-    res.send("hello delete");
-})
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server!");
-});
+// app.post("/hello",(req,res)=>{
+//     res.send("hello ji")
+// })
+// app.delete("/hello",(req,res)=>{
+//     res.send("hello delete");
+// })
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the server!");
+// });
+app.get("/test",(req,res,next)=>{
+    console.log("hello");
+    next();
+},
+(req,res)=>
+{
+    console.log("hello2");
+    res.send("hello hello");
+}
+)
 
 
 app.listen(3000,()=>{

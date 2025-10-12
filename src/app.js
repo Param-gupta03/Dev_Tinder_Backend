25a193ab2f1faae5f3c83f9b47feb90f17cd95a6
param gupta <paramgupta0305@gmail.com>
2025-10-12 12:35:37 +0530

    upto Listing api

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express=require("express");
 require("./config/database")
-const { adminAuth } = require("./middlewares/auth");
+const { userAuth } = require("./middlewares/auth");
 const { connectDB } = require("./config/database");
 const User =require("./models/user");
 const { object } = require("zod");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt")
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 app.post("/signup",async (req,res)=>{
     
     
@@ -33,99 +37,50 @@ app.post("/signup",async (req,res)=>{
 });
 
 
-app.post("/login",async (req,res)=>{
-    
-    
-    try{
-        const {emailId,password}=req.body;
-        const user=await User.findOne({emailId:emailId});
-        if(!user){
-            throw new Error("Invalid credentials");
-        }
-        const isPasswordValid= await bcrypt.compare(password,user.password);
-        if(isPasswordValid){
-            res.send("Login Successfull !!")
-        }
-        else{
-            throw new Error("Invalid credentails");
-            
-        }
-    
-    }
-    catch(err){
-        res.status(400).send("ERROR: " + err.message);
-    }
-    
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      const token=await user.getJWT();
+      
+      res.cookie("token", token);
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
 
-app.get("/user",async(req,res)=>{
-    const userEmail=req.body.emailId;
-    try{
-        const user = await User.findOne({emailId:userEmail});
-        if(user.length === 0){
-            res.status(400).send("user not found");
-        }
-        else{
-        res.send(user);
-        }
-        }
-    catch(err){
-        res.status(400).send("something went worng");
-    }
-})
+app.get("/profile",userAuth, async (req, res) => {
+  try {
+    const user=req.user;
 
-app.get("/feed",async(req,res)=>{
-    try{
-        const user = await User.find({});
-        if(user.length === 0){
-            res.status(400).send("user not found");
-        }
-        else{
-        res.send(user);
-        }
-        }
-    catch(err){
-        res.status(400).send("something went worng");
-    }
-})
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
 
-app.delete("/user", async (req,res)=>{
-    const userId = req.body.userId;
-    try{
-        const user = await User.findByIdAndDelete(userId);
-        res.send("User deleted ")
-    }
-     catch(err){
-        res.status(400).send("something went worng");
-    }
-})
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  const user = req.user;
+  // Sending a connection request
+  console.log("Sending a connection request");
+  res.send(user.firstName + "sent the connect request!");
+});
 
-app.patch("/user/:userId",async(req,res)=>{
-    const userId= req.params?.userId;
-    const data=req.body;
-    try{
-        const ALLOWED_UPDATES = ["userId","photoUrl","about","skills","age"];
-        const isUpataAllowed=Object.keys(data).every((k)=>
-            ALLOWED_UPDATES.includes(k)
-    );
-    if(!isUpataAllowed){
-        res.status(400).send("updata not allowed");
-    }
-    if(data?.skills.length >10){
-        throw new Error("Skills cannot be more then 10");
-    }
 
-     const user =  await User.findByIdAndUpdate({_id: userId},data,{
-            returnDocument: "after",
-            runValidators: true,
-        });
-        res.send("user updata")
-    }
-     catch(err){
-        res.status(400).send("updata failed" + err.massage);
-    }
-})
 
 
 connectDB()
